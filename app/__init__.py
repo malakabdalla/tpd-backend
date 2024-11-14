e94e3e30de0737666a5b39c1fd767cefe5a3495b
Samuel Moore <samoore@student.42london.com>
2024-11-14 18:46:19 +0000

    added blueprint

--- app/__init__.py
+++ app/__init__.py
@@ -2,10 +2,13 @@ from flask import Flask
 from flask_socketio import SocketIO
 from app.socket_handler import init_socket_handlers  # Import the function to initialize socket events
 from app.api import api_blueprint
+from flask_cors import CORS
 
 def create_app():
     app = Flask(__name__)
 
+	CORS(app)
+    
     # Initialize SocketIO with app and CORS allowed origins
     socketio = SocketIO(app, cors_allowed_origins="*", async_mode='threading')
 
