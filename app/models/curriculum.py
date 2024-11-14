9731ec629f6fd04898b3f6a9bc047bf5d61df6f0
Samuel Moore <samoore@student.42london.com>
2024-11-14 22:54:36 +0000

    get questions psql

--- app/models/curriculum.py
+++ app/models/curriculum.py
@@ -6,8 +6,8 @@ class Curriculum(db.Model):
     
     curriculum_id = db.Column(db.Integer, primary_key=True)
     module_id = db.Column(db.Integer, db.ForeignKey('module.module_id'))
-    phonics = db.Column(db.JSONB)
-    sight_words = db.Column(db.JSONB)
-    other_topics = db.Column(db.JSONB)
+    phonics = db.Column(db.JSON)
+    sight_words = db.Column(db.JSON)
+    other_topics = db.Column(db.JSON)
     created_at = db.Column(db.TIMESTAMP, default=datetime.utcnow)
     updated_at = db.Column(db.TIMESTAMP, default=datetime.utcnow, onupdate=datetime.utcnow)
