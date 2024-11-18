ed03c47f5d3bd60f96677eaaab84c9a54be3f522
Samuel Moore <samuelallister@outlook.com>
2024-11-18 07:43:19 +0000

    .

--- app/db/__init__.py
+++ app/db/__init__.py
@@ -4,6 +4,7 @@ from .questions import get_questions
 from .replace_question import replace_question
 from .exercise_by_id import get_questions_by_exercise_id
 from .homepage import get_homepage_data
+from app.config import logger
 
 db_blueprint = Blueprint('db', __name__)
 
@@ -28,4 +29,5 @@ def replace_question_route():
 @db_blueprint.route('/get_exercise_by_id', methods=['POST'])
 def get_question_by_exercise_id_route():
     exercise_id = request.args.get('exercise_id')
+    logger.debug(f"exercise_id: {exercise_id}")
     return get_questions_by_exercise_id(exercise_id)
