4418436afe1eba577835268f35b467e5343aa520
Samuel Moore <samuelallister@outlook.com>
2024-11-19 13:14:25 +0000

    add webhook

--- main.py
+++ main.py
@@ -1,6 +1,17 @@
 from app import create_app
+from flask import Flask, request, abort
+import subprocess
 
 app, socketio = create_app()
+
+@app.route('/webhook', methods=['POST'])
+def webhook():
+    if request.method == 'POST':
+        # Run the update script
+        subprocess.run(['/path/to/your/script/update_repo.sh'])
+        return 'Updated', 200
+    else:
+        abort(400)
 
 if __name__ == '__main__':
     socketio.run(app, host='0.0.0.0', port=5000, debug=True, allow_unsafe_werkzeug=True)
